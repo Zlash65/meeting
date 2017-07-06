c8e45499a3ded1e2f577447886d6928d57fcdbfe
Zlash65 <zarrar65@gmail.com>
2017-07-06 17:39:51 +0530

    added support to send emails

--- meeting/api.py
+++ meeting/api.py
@@ -1,7 +1,25 @@
 import frappe
+from frappe import _
 
 @frappe.whitelist()
 def send_invitation_emails(meeting):
 	meeting = frappe.get_doc("Meeting", meeting)
+	meeting.check_permission("email")
 
-	return meeting.status
+	if meeting.status == "Planned":
+		frappe.sendmail(
+			recipients = [x.attendee for x in meeting.attendees],
+			sender = frappe.session.user,
+			subject = meeting.title,
+			message = meeting.invitation_message,
+			reference_doctype = meeting.doctype,
+			reference_name = meeting.name
+			# as_bulk=True
+		)
+
+		meeting.status = "Invitation Sent"
+		meeting.save()
+		frappe.msgprint(_("Invitation Sent"))
+
+	else:
+		frappe.msgprint(_("Meeting Status must be planned"))
