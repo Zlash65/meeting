e87ef6acbf7bb7c690fbe34123408d703bb38933
Zlash65 <zarrar65@gmail.com>
2017-07-06 17:42:07 +0530

    removed server side checking of duplicates

--- meeting/meeting/doctype/meeting/meeting.py
+++ meeting/meeting/doctype/meeting/meeting.py
@@ -4,21 +4,15 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class Meeting(Document):
 	def validate(self):
 		""" Set missing names and avoid duplication """
-		found = []
 		for attendee in self.attendees:
 			if not attendee.full_name:
-				attendee.full_name = get_full_name(attendee.attendee)
-
-			if attendee.attendee in found:
-				frappe.throw(_("Attendee {0} already in list").format(attendee.attendee))
-
-
-			found.append(attendee.attendee)			
+				attendee.full_name = get_full_name(attendee.attendee)	
 
 @frappe.whitelist()
 def get_full_name(attendee):
