5c1934bcc3ca1375f21a18959e9c6290b0d31548
Zlash65 <zarrar65@gmail.com>
2017-07-07 13:19:48 +0530

    duplication and deletion bug fixed

--- meeting/meeting/doctype/meeting/meeting.js
+++ meeting/meeting/doctype/meeting/meeting.js
@@ -2,8 +2,16 @@
 // For license information, please see license.txt
 
 var attendeeList =[];
-for(i=0;i<cur_frm.doc.attendees.length;i++){
-	attendeeList.push(cur_frm.doc.attendees[i].attendee);
+if(cur_frm.doc.attendees != undefined){
+	attendeeList = getName();
+}
+
+function getName(){
+	var x = [];
+	for(i=0;i<cur_frm.doc.attendees.length;i++){
+		x.push(cur_frm.doc.attendees[i].attendee);
+	}
+	return x;
 }
 
 frappe.ui.form.on("Meeting", {
@@ -43,6 +51,18 @@ frappe.ui.form.on('Meeting Attendee', {
 					frappe.model.set_value(cdt, cdn, "full_name", r.message);
 				}
 			});
+		}
+	},
+	attendees_remove: function(frm){
+		var x = getName();
+		removed = attendeeList.filter( function( el ) {
+			return x.indexOf( el ) < 0;
+		});
+		for(i=0;i<attendeeList.length;i++){
+			if(removed[0] == attendeeList[i]){
+				attendeeList.splice(i,1);
+				break;
+			}
 		}
 	}
 });
