01e5c4c5fcf162f7525cf02e4f889ba9678ab284
Zlash65 <zarrar65@gmail.com>
2017-07-10 16:14:53 +0530

    ToDo linkage functionality added

--- meeting/meeting/doctype/meeting/meeting.py
+++ meeting/meeting/doctype/meeting/meeting.py
@@ -9,10 +9,60 @@ from frappe.model.document import Document
 
 class Meeting(Document):
 	def validate(self):
+		""" Reflect changes when Doctypes are being updated or created or so """
+		self.validate_attendees()
+
+	def validate_attendees(self):
 		""" Set missing names and avoid duplication """
 		for attendee in self.attendees:
 			if not attendee.full_name:
 				attendee.full_name = get_full_name(attendee.attendee)
+
+	def on_update(self):
+		""" Sync ToDos appropraitely """
+		self.sync_todos()
+
+	def sync_todos(self):
+		""" Sync ToDos for assignments """
+		# todos_added = [minute.todo for minute in self.minutes if minute.todo]
+		# todos_added = frappe.get_all("ToDo")
+		todos_added = [todo.name for todo in 
+			frappe.get_all("ToDo",
+				filters={
+					"reference_type": self.doctype,
+					"reference_name": self.name,
+					"assigned_by": ""
+				})
+			]
+
+		# print todos_added
+
+		for minute in self.minutes:
+			if minute.assigned_to and minute.status=="Open":
+				# print self.name
+				if not minute.todo:
+					todo = frappe.get_doc({
+						"doctype": "ToDo",
+						"description": minute.description,
+						"reference_type": self.doctype,
+						"reference_name": self.name,
+						"owner": minute.assigned_to
+					})
+					todo.insert()
+					# print todo.description
+					minute.db_set("todo", todo.name, update_modified=False)	
+					# minute.todo = todo.name
+
+				else:
+					todos_added.remove(minute.todo)
+			else:
+				minute.db_set("todo", None, update_modified=False)
+
+		for todo in todos_added:
+			todo = frappe.get_doc("ToDo", todo)
+			todo.flags.from_meeting	= True
+			# frappe.delete_doc("ToDo", todo)
+			todo.delete()
 
 @frappe.whitelist()
 def get_full_name(attendee):
